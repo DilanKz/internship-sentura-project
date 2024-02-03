0d585a980db9b35d3cf9b71b04b2421787268c4d
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-03 10:29:45 +0530

    Update Main.java

--- Backend/src/main/java/org/example/Main.java
+++ Backend/src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
-import okhttp3.Callback;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
+
 import java.io.IOException;
 /**
  * @author Dilan
@@ -11,30 +8,61 @@ import java.io.IOException;
  */
 public class Main {
 
-    private static String url = "https://70bd3ab9ff7a4a9ebca6c3f9f6e06e36.weavy.io/api/users/u256/tokens";
-
-    public static void main(String[] args) {
-        OkHttpClient client = new OkHttpClient();
-        String bearerToken = "wys_fT64v1TfZi1mAG1xzrPydS2nDvUXj90EzXTY";
-
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final String BASE_URL = "http://localhost:8080/api/data";
+    private static final String bearerToken = "wys_fT64v1TfZi1mAG1xzrPydS2nDvUXj90EzXTY";
+    
+    public static String getData(String id) throws IOException {
+        String url = BASE_URL + "/" + id;
         Request request = new Request.Builder()
                 .url(url)
                 .addHeader("Authorization", "Bearer " + bearerToken)
                 .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                System.out.println("Response: " + response.body().string());
-            }
-
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-        });
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
     }
 
+    public static String addData(String newData) throws IOException {
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), newData);
+        Request request = new Request.Builder()
+                .url(BASE_URL)
+                .addHeader("Authorization", "Bearer " + bearerToken)
+                .post(requestBody)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static String deleteData(String id) throws IOException {
+        String url = BASE_URL + "/" + id;
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "Bearer " + bearerToken)
+                .delete()
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static String updateData(String id, String updatedData) throws IOException {
+        String url = BASE_URL + "/" + id;
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), updatedData);
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "Bearer " + bearerToken)
+                .put(requestBody)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 
 }
